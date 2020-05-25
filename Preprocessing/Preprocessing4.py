a3977d092bcff72a26735fe9d76cf17c9a5f4b28
Henrik Mjaaland <noggerfog@hotmail.com>
2020-05-25 12:21:54 +0200

    Update Preprocessing4.py

--- Preprocessing/Preprocessing4.py
+++ Preprocessing/Preprocessing4.py
@@ -11,12 +11,6 @@ bigdata = bigdata.dropna(axis = 1, how = 'all')
 #drop rows with less than 63 non-NAN values:
 bigdata = bigdata.dropna(thresh=7) #t = 0 gives 1594 entries, t = 5 gives 1592 entries, t=10 gives 793 entries
 
-#movel label to end:
-#cols = list(df.columns.values) #Make a list of all of the columns in the df
-#cols.pop(cols.index('b')) #Remove b from list
-#cols.pop(cols.index('x')) #Remove x from list
-#df = df[cols+['b','x']]
-
 #display head
 bigdata.head()
 
