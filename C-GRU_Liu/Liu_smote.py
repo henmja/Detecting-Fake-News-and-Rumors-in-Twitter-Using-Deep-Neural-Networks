372e776b2aff7ba7bb18652124abf5a89f4301a6
Henrik Mjaaland <noggerfog@hotmail.com>
2020-05-29 12:24:04 +0200

    Update Liu_smote.py

--- C-GRU_Liu/Liu_smote.py
+++ C-GRU_Liu/Liu_smote.py
@@ -43,7 +43,7 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 import numpy as np
 
-df = pd.read_pickle("bigdata_timeseries.pkl")
+df = pd.read_pickle("../Preprocessing/bigdata_timeseries.pkl")
 print('before')
 
 df['created_at'] = df['created_at'].astype(str)
