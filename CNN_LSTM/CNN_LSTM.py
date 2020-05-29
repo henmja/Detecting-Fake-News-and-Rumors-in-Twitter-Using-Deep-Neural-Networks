b007367da69c84092a7f6d257ba9b81b791361c1
Henrik Mjaaland <noggerfog@hotmail.com>
2020-05-29 12:52:53 +0200

    Update CNN_LSTM.py

--- CNN_LSTM/CNN_LSTM.py
+++ CNN_LSTM/CNN_LSTM.py
@@ -1,16 +1,23 @@
 #coding: utf-8
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import json
 import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score
 from langdetect import detect
 from  tqdm import tqdm
 import nltk
 nltk.download('stopwords')
-
+from keras.layers import Bidirectional
+from keras.layers import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers.convolutional import MaxPooling2D
 import nltk
 import re
 from tqdm import tqdm_notebook
 from nltk.corpus import stopwords
-
+from tensorflow import keras
 from tensorflow.keras import regularizers, initializers, optimizers, callbacks
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -20,28 +27,36 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers import Embedding
-from keras.layers import LSTM
-from keras.layers import Conv1D
+from keras.callbacks import EarlyStopping
+from nltk.stem.snowball import SnowballStemmer
+from textblob import TextBlob
 from keras.layers import GlobalMaxPool1D
-from keras.layers import Dropout
-from keras.layers import InputLayer
-from sklearn.metrics import classification_report
-from imblearn.under_sampling import RandomUnderSampler
-from sklearn.metrics import roc_auc_score
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import InputLayer
+from tensorflow.keras.layers import Input, Dense, Embedding, Dropout, GRU, Concatenate
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Bidirectional
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
 import pandas as pd
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 import numpy as np
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import GridSearchCV
 
 df = pd.read_pickle("../Preprocessing/bigdata_timeseries.pkl")
+print('before')
+
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("english")
+df['text'] = df['text'].apply(lambda x: stemmer.stem(x)) # Stem every word.
+print('corrected')
 
 df['created_at'] = df['created_at'].astype(str)
 df['followers'] = df['followers'].astype(str)
 df['following'] = df['following'].astype(str)
 
 cat_Cols = ['text', 'name_user','name_zero_user_mentions_entities', 'location_user', 'description_user', 'contributors_enabled_user', 'default_profile_image_user', 'default_profile_user', 'favorited', 'follow_request_sent_user', 'following_user', 'geo_enabled_user', 'has_extended_profile_user', 'id', 'id_str', 'id_str_user', 'id_str_zero_user_mentions_entities', 'id_user', 'id_zero_user_mentions_entities', 'is_quote_status', 'is_translation_enabled_user', 'is_translator_user', 'lang', 'location_user', 'name_user', 'notifications_user', 'possibly_sensitive', 'possibly_sensitive_appealable', 'profile_background_color_user', 'profile_background_tile_user', 'profile_link_color_user', 'profile_sidebar_border_color_user', 'profile_sidebar_fill_color_user', 'profile_text_color_user', 'profile_use_background_image_user', 'protected_user', 'retweeted', 'screen_name_user', 'screen_name_zero_user_mentions_entities', 'translator_type_user', 'truncated', 'verified_user']
+
 
 cat_Features =df['text']
 print(str(df['text']).encode('utf-8').decode('latin-1'))
@@ -59,16 +74,14 @@ for idx in num_Cols:
     print(idx)
     num_Features.append(df[idx])
 print(type(num_Features[0]))
-
-import pandas as pd
+import pandas as pd	
 from sklearn import preprocessing
 
 for i,col in enumerate(num_Features):
-    if i == 0:
-        mat = col
+    if i == 0:	
+        mat = col	
     else:
         mat = pd.concat([mat, col], axis=1)
-
 x = mat.values #returns a numpy array
 min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-0.5,0.5))
 x_scaled = min_max_scaler.fit_transform(x)
@@ -80,7 +93,9 @@ for col in num_Norm.keys():
 strings = [] 
 stop_words = set(stopwords.words('english')) 
 
-for line in tqdm_notebook(cat_Features, total=df.shape[0]): 
+print(df.shape)
+for i,line in enumerate(tqdm_notebook(cat_Features, total=df.shape[0])): 
+    line = re.sub(r'^https?:\/\/.*[\r\n]*', '', line, flags=re.MULTILINE)
     line = line.replace("'","")
     line = line.replace("\[","")
     line = line.replace(":","")
@@ -119,33 +134,18 @@ term_Index = tokenizer.word_index
 print('Number of Terms:', len(term_Index))
 
 
-sen_Len = 162 # max length of each sentences, including padding
-tok_Features = pad_sequences(sequences, padding = 'post', maxlen = sen_Len-111)
+sen_Len = 98 # max length of each sentences, including padding
+tok_Features = pad_sequences(sequences, padding = 'post', maxlen = sen_Len)
 print('Shape of tokenized features tensor:', tok_Features.shape)
 
 indices = np.arange(tok_Features.shape[0])
 np.random.shuffle(indices)
 time_series = df['created_at_retweets']
 time_series.reset_index(drop=True, inplace=True)
-print('before')
-for i,time in enumerate(time_series):
-    if isinstance(time,float):
-        print(i)
-        print(time)
-print('etter')
-print(type(time_series))
+
 time_series = time_series[indices]
-print('before2')
-print(time_series)
-for i,time in enumerate(time_series):
-    print(i)
-    print(type(time))
-    print('etter2')
 tok_Features = tok_Features[indices]
 labels = label[indices]
-print(num_Norm)
-print(num_Norm.shape)
-print(type(num_Norm))
 
 test_Perc = 0.2 #20% data used for testing
 num_validation_samples = int(test_Perc*tok_Features.shape[0])
@@ -154,8 +154,17 @@ time_series_Train = time_series[: -num_validation_samples]
 num_Norm_Train = num_Norm[: -num_validation_samples]
 target_Train = labels[: -num_validation_samples]
 features_Val = tok_Features[-num_validation_samples: ]
+
 num_Norm_Val = num_Norm[-num_validation_samples: ]
+num_Norm_Val = num_Norm_Val.to_numpy()
+print('NUM NORM VAL')
+print(num_Norm_Val)
 time_series_Val = time_series[-num_validation_samples: ]
+
+print(time_series_Train.shape)
+print('TIME SERIES VAL')
+print(time_series_Val)
+print(time_series_Val.shape)
 target_Val = labels[-num_validation_samples: ]
 print('Number of records in each attribute:')
 print('training: ', target_Train.sum(axis=0))
@@ -163,7 +172,7 @@ print('validation: ', target_Val.sum(axis=0))
 
 
 emb_Dim = 100 # embedding dimensions for word vectors
-glove = '../LSTM/glove.6B.'+str(emb_Dim)+'d.txt'
+glove = 'glove.6B.'+str(emb_Dim)+'d.txt'
 emb_Ind = {}
 f = open(glove, encoding='utf8')
 print('Loading Glove \n')
@@ -180,135 +189,163 @@ for term, i in term_Index.items():
     if emb_Vec is not None:
         emb_Mat[i] = emb_Vec
 print("Done")
-print('SHAPE EMB MAT')
-print(emb_Mat.shape)
-print(type(emb_Mat))
-print('SHAPE TIME SERIES')
-print(time_series_Train.shape)
-print(type(time_series_Train))
+
 maxLen = 0
 maxIndex = 0
-
 time_series_Train = np.asarray(time_series_Train)
-
-for i,times in enumerate(time_series_Train):
-
-    time_series_Train[i] = np.array(time_series_Train[i])
-max_len = max([len(x) for x in time_series_Train]) 
-for i,times in enumerate(time_series_Train):
-    time_series_Train[i] = np.pad(times,(0,max_len-len(times)), 'constant')
-time_series_Mat = np.zeros((len(time_series_Train),max_len))
-for i, times in enumerate(time_series_Train):
-    for j, time in enumerate(time_series_Train[i]):
-        time_series_Mat[i,j] = time
-features_Train = np.concatenate([features_Train,time_series_Mat],axis=1)
-
-features_Train = np.concatenate([features_Train,num_Norm_Train],axis=1)
-
-undersample = RandomUnderSampler()
-print(target_Train)
-print(target_Train.shape)
-print(type(target_Train))
-features_Train_Resampled, target_Train_Resampled = undersample.fit_resample(features_Train, target_Train)
-
-target_Train_Resampled = to_categorical(target_Train_Resampled)
-
-for feat in features_Train:
-    print('blah')
-    print(feat)
-
-def create_model():
-	# create model
-    model = Sequential()
-    model.add(InputLayer((sen_Len,),dtype='int32'))
-    e = Embedding(len(term_Index) + 1, emb_Dim, weights=[emb_Mat], input_length=sen_Len, trainable=False)
-    print('SEN LEN')
-    print(sen_Len)
-    print('TERM INDEX')
-    print(len(term_Index))
-    model.add(e)
-    model.add(Conv1D(128, 5, activation='relu'))
-    model.add(GlobalMaxPool1D())
-    model.add(Dropout(0.1))
-    model.add(Dense(158, activation='relu'))
-    model.add(Dropout(0.1))
-    model.add(Dense(2, activation='sigmoid'))
-
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics = ['acc'])
-    return model
-
-gridmodel = KerasClassifier(build_fn=create_model,epochs=10, batch_size=5, verbose=0)
-
-batch_size = [10, 20, 40, 60, 80, 100]
-epochs = [10, 50, 100]
-
-model = Sequential()
-model.add(InputLayer((sen_Len,),dtype='int32'))
-e = Embedding(len(term_Index) + 1, emb_Dim, weights=[emb_Mat], input_length=sen_Len, trainable=False)
-print('SEN LEN')
-print(sen_Len)
-print('TERM INDEX')
-print(len(term_Index))
-model.add(e)
-model.add(Conv1D(128, 5, activation='relu'))
-model.add(GlobalMaxPool1D())
-model.add(Dropout(0.1))
-model.add(Dense(159, activation='relu'))
-model.add(Dropout(0.1))
-model.add(Dense(2, activation='sigmoid'))
-
-
-
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics = ['acc'])
-
-
-print(max_len)
-history = model.fit(features_Train_Resampled, target_Train_Resampled, epochs = 5, batch_size=32, validation_split=0.20)
 time_series_Val = np.asarray(time_series_Val)
-
+for i,times in enumerate(time_series_Train):
+    time_series_Train[i] = np.array(time_series_Train[i])
+print(time_series_Train.shape)
+print(time_series_Val.shape)
 for i,times in enumerate(time_series_Val):
     time_series_Val[i] = np.array(time_series_Val[i])
-max_len_val = max([len(x) for x in time_series_Val])
+print(time_series_Train.shape)
+print(time_series_Val.shape)
+max_len = max([len(x) for x in time_series_Train])
+
+for i,times in enumerate(time_series_Train):
+	
+    time_series_Train[i] = np.pad(times,(0,max_len-len(times)), 'constant')
+
+time_series_Mat = np.zeros((len(time_series_Train),max_len))
+	
+for i, times in enumerate(time_series_Train):
+	
+    for j, time in enumerate(time_series_Train[i]):
+        time_series_Mat[i,j] = time
+
 for i,times in enumerate(time_series_Val):
-    time_series_Val[i] = np.pad(times,(0,max_len_val-len(times)), 'constant')
-time_series_val_Mat = np.zeros((len(time_series_Val),max(max_len,max_len_val)))
+
+    time_series_Val[i] = np.pad(times,(0,max_len-len(times)), 'constant')
+
+time_series_Mat_Val = np.zeros((len(time_series_Val),max_len))
+
 for i, times in enumerate(time_series_Val):
+
     for j, time in enumerate(time_series_Val[i]):
-        time_series_val_Mat[i,j] = time
-print(time_series_val_Mat)
-print(time_series_val_Mat.shape)
-combined_Val = np.concatenate([features_Val,time_series_val_Mat],axis=1)
-combined_Val = np.concatenate([combined_Val,num_Norm_Val],axis=1)
-target_Val = np.argmax(target_Val, axis=1)
+        time_series_Mat_Val[i,j] = time
 
-predictions = model.predict(combined_Val)
+features_Train = np.concatenate([features_Train,time_series_Mat],axis=1)
 
-predictions_bool = np.argmax(predictions, axis=1)
-print(predictions_bool.shape)
-print(classification_report(target_Val, predictions_bool,digits=3))
-predictions_prob = model.predict_proba(combined_Val)
+features_Val = np.concatenate([features_Val,time_series_Mat_Val],axis=1)
 
-target_Val = to_categorical(target_Val)
-auc = roc_auc_score(target_Val, predictions_prob)
+features_Train = np.asarray(features_Train)
+features_Val = np.asarray(features_Val)
+for i,features in enumerate(features_Train):
+    features_Train[i] = np.array(features_Train[i])
+print(features_Train.shape)
+print(features_Val.shape)
+for i,features in enumerate(features_Val):
+    features_Val[i] = np.array(features_Val[i])
+print(features_Train.shape)
+print(features_Val.shape)
+pad_len = max([len(x) for x in features_Train])
+
+for i,features in enumerate(features_Train):
+
+    features_Train[i] = np.pad(features,(0,pad_len-len(features)), 'constant')
+
+features_Pad = np.zeros((len(features_Train),pad_len))
+
+for i, features in enumerate(features_Train):
+
+    for j, feature in enumerate(features_Train[i]):
+        features_Pad[i,j] = feature
+
+for i,features in enumerate(features_Val):
+
+    features_Val[i] = np.pad(features,(0,pad_len-len(features)), 'constant')
+
+features_Pad_Val = np.zeros((len(features_Val),pad_len))
+
+for i, features in enumerate(features_Val):
+
+    for j, feature in enumerate(features_Val[i]):
+        features_Pad_Val[i,j] = feature
+
+print(features_Pad.shape)
+
+from keras.layers import Input, Bidirectional, Embedding, LSTM, Dense, merge, concatenate, GRU, Conv1D, Flatten, Dropout
+from keras.models import Model
+from keras import metrics
+
+main_input = Input(shape=(pad_len,), dtype='int32', name='main_input')
+
+x = Embedding(output_dim=512, input_dim=20000, input_length=pad_len)(main_input)
+
+gru_out = Bidirectional(LSTM(32))(x)
+gru_out = Dropout(0.5)(gru_out)
+auxiliary_output = Dense(2, activation='sigmoid', name='aux_output')(gru_out)
+
+auxiliary_input = Input(shape=(13,), name='aux_input')
+e = Embedding(output_dim=512, input_dim=20000, input_length=13)(auxiliary_input)
+cnn_out = Conv1D(32,3)(e)
+cnn_out = Dropout(0.5)(cnn_out)
+cnn_out = Flatten()(cnn_out)
+x = concatenate([gru_out, cnn_out])
+
+x = Dense(64, activation='relu')(x)
+x = Dense(64, activation='relu')(x)
+x = Dense(64, activation='relu')(x)
+
+main_output = Dense(2, activation='sigmoid', name='main_output')(x)
+
+model = Model(input=[main_input, auxiliary_input], output=[main_output, auxiliary_output])
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy',loss_weights=[1., 0.2],metrics=[metrics.AUC()])
+num_Norm_Train = num_Norm_Train.to_numpy()
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
+print(time_series_Mat.shape)
+model.fit([features_Pad,num_Norm_Train], [target_Train, target_Train], nb_epoch=2, batch_size=16,callbacks=[es])
+
+print(time_series_Mat_Val.shape)
+print(num_Norm_Val.shape)
+print(type(time_series_Mat_Val))
+print(type(num_Norm_Val))
+#predictions = model.predict(np.array([[time_series_Mat_Val,num_Norm_Val]]))
+predictions = model.predict([features_Pad_Val,num_Norm_Val])
+from sklearn.metrics import classification_report
+predictions_bool = np.argmax(predictions[0], axis=1)
 
 import sklearn.metrics as metrics
-y_pred = (predictions > 0.5)
 
-matrix = metrics.confusion_matrix(target_Val.argmax(axis=1), y_pred.argmax(axis=1))
+print(len(predictions))
+predictions = np.asarray(predictions)
+
+y_pred = (predictions > 0.5)
+print('target_Val shape')
+print(target_Val.shape)
+print('y_pred shape')
+print(y_pred.shape)
+print('predictions shape')
+print(predictions.shape)
+print(type(y_pred[0]))
+print(type(y_pred[1]))
+y_predictions = y_pred[0]+y_pred[1]
+matrix = metrics.confusion_matrix(target_Val.argmax(axis=1), predictions[0].argmax(axis=1))
+
+print('predictions')
+print(predictions[0])
+print(predictions[0].argmax(axis=1))
+print('y_pred')
+print(y_pred[0])
+print(y_pred[0].argmax(axis=1))
+print(matrix)
 micro = (matrix[0,0]+matrix[1,1])/(matrix[0,0]+matrix[1,1]+matrix[0,1]+matrix[1,0])
 print('micro')
 print(micro)
 print('macro')
 macro = (matrix[0,0]/(matrix[0,0]+matrix[1,0])+matrix[1,1]/(matrix[1,1]+matrix[1,0]))/2
 print(macro)
+
+print(classification_report(target_Val.argmax(axis=1), predictions_bool,digits=3))
+
+predictions_prob = model.predict_proba([time_series_Mat_Val,num_Norm_Val])
+auc = roc_auc_score(target_Val, predictions_prob)
 print('auc')
 print(auc)
-
-import sklearn.metrics as metrics
-y_pred = (predictions > 0.5)
-matrix = metrics.confusion_matrix(target_Val.argmax(axis=1), y_pred.argmax(axis=1))
-print(matrix)
 
 import matplotlib.pyplot as plt
 #%matplotlib inline
@@ -337,3 +374,15 @@ plt.legend()
 plt.savefig("accuracy"+".png", bbox_inches='tight')
 
 print(matrix)
+micro = (matrix[0,0]+matrix[1,1])/(matrix[0,0]+matrix[1,1]+matrix[0,1]+matrix[1,0])
+print('micro')
+print(micro)
+print('macro')
+macro = (matrix[0,0]/(matrix[0,0]+matrix[1,0])+matrix[1,1]/(matrix[1,1]+matrix[1,0]))/2
+print(macro)
+predictions_prob = model.predict_proba([time_series_Mat_Val,num_Norm_Val])
+print(classification_report(target_Val, predictions_prob,digits=3))
+target_Val = np.argmax(target_Val, axis=1)
+auc = roc_auc_score(target_Val, predictions_bool)
+print('auc')
+print(auc)
