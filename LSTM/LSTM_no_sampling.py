88d5b84598a7d5c9085f464b86e8a4cf720c9ff0
Henrik Mjaaland <noggerfog@hotmail.com>
2020-05-29 13:43:06 +0200

    Update LSTM_no_sampling.py

--- LSTM/LSTM_no_sampling.py
+++ LSTM/LSTM_no_sampling.py
@@ -292,7 +292,7 @@ macro = (matrix[0,0]/(matrix[0,0]+matrix[1,0])+matrix[1,1]/(matrix[1,1]+matrix[1
 print(macro)
 print('auc')
 print(auc)
-
+print(matrix)
 import matplotlib.pyplot as plt
 #%matplotlib inline
 
