b60106da80255bd433b38dde2d5c97459634634b
Henrik Mjaaland <noggerfog@hotmail.com>
2020-06-01 10:21:40 +0200

    Update NB.py

--- NB/NB.py
+++ NB/NB.py
@@ -35,7 +35,7 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 import numpy as np
 
-df = pd.read_pickle("../Preprocessing/bigdata_timeseries.pkl")
+df = pd.read_pickle("../Preprocessing/bigdata_preprocessed.pkl")
 
 df['created_at'] = df['created_at'].astype(str)
 df['followers'] = df['followers'].astype(str)
