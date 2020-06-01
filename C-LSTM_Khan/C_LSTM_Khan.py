e640a97750f02ca917f50c832ab6333dff241898
Henrik Mjaaland <noggerfog@hotmail.com>
2020-06-01 10:14:08 +0200

    Update C_LSTM_Khan.py

--- C-LSTM_Khan/C_LSTM_Khan.py
+++ C-LSTM_Khan/C_LSTM_Khan.py
@@ -39,7 +39,7 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 import numpy as np
 
-df = pd.read_pickle("../Preprocessing/bigdata_timeseries.pkl")
+df = pd.read_pickle("../Preprocessing/bigdata_preprocessed.pkl")
 df.text.apply(lambda txt: ''.join(TextBlob(txt).correct()))
 stemmer = SnowballStemmer("english")
 df['text'] = df['text'].apply(lambda x: stemmer.stem(x)) # Stem every word.
