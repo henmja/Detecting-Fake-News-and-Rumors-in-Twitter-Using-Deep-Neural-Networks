08e90010891e84317d198fdcbd24a4b8d131187f
Henrik Mjaaland <noggerfog@hotmail.com>
2020-06-09 21:44:56 +0200

    Update LSTM_Khan_under.py

--- BiLSTM_Khan/LSTM_Khan_under.py
+++ BiLSTM_Khan/LSTM_Khan_under.py
@@ -37,7 +37,7 @@ from imblearn.under_sampling import RandomUnderSampler
 
 df = pd.read_pickle("/local/home/henrikm/Fakenews_Classification/Preprocessing/bigdata_preprocessed.pkl")
 print('before')
-#df.text.apply(lambda txt: ''.join(TextBlob(txt).correct()))
+df.text.apply(lambda txt: ''.join(TextBlob(txt).correct()))
 stemmer = SnowballStemmer("english")
 df['text'] = df['text'].apply(lambda x: stemmer.stem(x)) # Stem every word.
 print('corrected')
