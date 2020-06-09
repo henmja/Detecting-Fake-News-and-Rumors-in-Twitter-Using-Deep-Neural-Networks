1b5bdcdf0608f356ff1d6b3fc438e5d8610ca6bd
Henrik Mjaaland <noggerfog@hotmail.com>
2020-06-09 22:15:42 +0200

    Update T_Test_smote.py

--- T_Test/T_Test_smote.py
+++ T_Test/T_Test_smote.py
@@ -1,33 +1,33 @@
 import pandas as pd
 import numpy as np
 try:
-    BiLSTM_Accuracies = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/BiLSTM_smote_accuracies.pkl')
-    BiLSTM_Accuracies = np.mean(BiLSTM_Accuracies,axis=1)
+    BiLSTM_proba = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/BiLSTM_smote_proba.pkl')
+    BiLSTM_proba = np.mean(BiLSTM_proba,axis=1)
 except FileNotFoundError:
     pass
 try:
-    C_LSTM_Accuracies = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/C-LSTM_smote_accuracies.pkl')
-    C_LSTM_Accuracies = np.mean(C_LSTM_Accuracies,axis=1)
+    C_LSTM_proba = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/C-LSTM_smote_proba.pkl')
+    C_LSTM_proba = np.mean(C_LSTM_proba,axis=1)
 except FileNotFoundError:
     pass
 try:
-    C_LSTM_Func_Accuracies = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/CNN_LSTM_smote_accuracies.pkl')  
-    C_LSTM_Func_Accuracies = np.mean(C_LSTM_Func_Accuracies,axis=1)
+    C_LSTM_Func_proba = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/CNN_LSTM_smote_proba.pkl')  
+    C_LSTM_Func_proba = np.mean(C_LSTM_Func_proba,axis=1)
 except FileNotFoundError:
     pass
 try:
-    CNN_Accuracies = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/CNN_smote_accuracies.pkl')
-    CNN_Accuracies = np.mean(CNN_Accuracies,axis=1) 
+    CNN_proba = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/CNN_smote_proba.pkl')
+    CNN_proba = np.mean(CNN_proba,axis=1) 
 except FileNotFoundError:
     pass
 try:
-    Khan_LSTM_Accuracies = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/Khan_BiLSTM_smote_accuracies.pkl')
-    Khan_LSTM_Accuracies = np.mean(Khan_LSTM_Accuracies,axis=1)
+    Khan_LSTM_proba = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/Khan_BiLSTM_smote_proba.pkl')
+    Khan_LSTM_proba = np.mean(Khan_LSTM_proba,axis=1)
 except FileNotFoundError:
     pass
 try:
-    Khan_C_LSTM_Accuracies = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/Khan_C_LSTM_smote_accuracies.pkl')
-    Khan_C_LSTM_Accuracies = np.mean(Khan_C_LSTM_Accuracies,axis=1)
+    Khan_C_LSTM_proba = pd.read_pickle('/local/home/henrikm/Fakenews_Classification/T_Test/Khan_C_LSTM_smote_proba.pkl')
+    Khan_C_LSTM_proba = np.mean(Khan_C_LSTM_proba,axis=1)
 except FileNotFoundError:
     pass
 import numpy as np
@@ -67,7 +67,7 @@ seed(1)
 
 alpha = 0.01
 try:
-    t_stat, df, cv, p = independent_ttest(C_LSTM_Func_Accuracies, BiLSTM_Accuracies, alpha)
+    t_stat, df, cv, p = independent_ttest(C_LSTM_Func_proba, BiLSTM_proba, alpha)
     print('T-test for CNN+LSTM and BiLSTM')
     print('t=%.3f, df=%d, cv=%.3f, p=%.3f' % (t_stat, df, cv, p))
     # interpret via critical value
@@ -89,7 +89,7 @@ try:
 except NameError:
     pass
 try:
-    t_stat, df, cv, p = independent_ttest(C_LSTM_Accuracies, BiLSTM_Accuracies, alpha)
+    t_stat, df, cv, p = independent_ttest(C_LSTM_proba, BiLSTM_proba, alpha)
     print('T-test for C-LSTM and BiLSTM')
     print('t=%.3f, df=%d, cv=%.3f, p=%.3f' % (t_stat, df, cv, p))
     # interpret via critical value
@@ -111,7 +111,7 @@ try:
 except NameError:
     pass
 try:
-    t_stat, df, cv, p = independent_ttest(CNN_Accuracies, BiLSTM_Accuracies, alpha)
+    t_stat, df, cv, p = independent_ttest(CNN_proba, BiLSTM_proba, alpha)
     print('T-test for CNN and BiLSTM')
     print('t=%.3f, df=%d, cv=%.3f, p=%.3f' % (t_stat, df, cv, p))
     # interpret via critical value
@@ -133,7 +133,7 @@ try:
 except NameError:
     pass
 try:
-    t_stat, df, cv, p = independent_ttest(Khan_LSTM_Accuracies, BiLSTM_Accuracies, alpha)
+    t_stat, df, cv, p = independent_ttest(Khan_LSTM_proba, BiLSTM_proba, alpha)
     print('T-test for BiLSTM (Khan) and BiLSTM')
     print('t=%.3f, df=%d, cv=%.3f, p=%.3f' % (t_stat, df, cv, p))
     # interpret via critical value
@@ -155,7 +155,7 @@ try:
 except NameError:
     pass
 try:
-    t_stat, df, cv, p = independent_ttest(Khan_C_LSTM_Accuracies, BiLSTM_Accuracies, alpha)
+    t_stat, df, cv, p = independent_ttest(Khan_C_LSTM_proba, BiLSTM_proba, alpha)
     print('T-test for C-LSTM (Khan) and BiLSTM')
     print('t=%.3f, df=%d, cv=%.3f, p=%.3f' % (t_stat, df, cv, p))
 # interpret via critical value
